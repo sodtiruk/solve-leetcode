0b1e4249943f43631482a511411d91d1bbf0707a
sodtiruk <sodtiruk22@gmail.com>
2024-12-30 17:08:49 +0700

    fix: 2-AddTwoNumber node2 not travel link list

--- 2-AddTwoNumber/main.py
+++ 2-AddTwoNumber/main.py
@@ -14,23 +14,24 @@ class Solution(object):
 
         head = ListNode()
         l3 = head
-        carry = 0 
+        carry = 0
         while l1 != None or l2 != None:
             
-            if l1 == None: l1v = 0
+            if l1 == None:l1v = 0 
             else: l1v = l1.val
-            if l2 == None: l2v = 0
+            if l2 == None:l2v = 0
             else: l2v = l2.val
 
             total = l1v + l2v + carry
             carry = total//10
-            lastDigit = total%10
+            lastDigit = total%10 
 
             l3.next = ListNode(lastDigit)
             l3 = l3.next
 
             if l1 != None:
                 l1 = l1.next
+            if l2 != None:
                 l2 = l2.next
 
         if carry > 0:
